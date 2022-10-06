086ba7cdab7c5efc5e482170dc54bed05b0aedd6
tuz22 <seungji.dev@gmail.com>
2022-10-06 20:14:16 +0900

    practice

--- practice/p-constructor.js
+++ practice/p-constructor.js
@@ -18,4 +18,44 @@ console.log(product1);
 console.log(product2);
 console.log(product1.부가세());
 
+// 2. 코드의 출력 결과는?
+function Parent(){
+  this.name = 'Kim';
+}
+var a = new Parent(); // a = { name : 'Kim' }
 
+a.__proto__.name = 'Park'; // 부모 prototype에 { name : 'Park' } 추가
+console.log(a.name) // 'Kim'
+
+// 3. 이름이 출력되지 않는 이유는?
+function Student(이름, 나이){
+  this.name = 이름;
+  this.age = 나이;
+}
+
+Student.prototype.sayHi = () => {
+    console.log('안녕 나는 ' + this.name + '이야');
+  }
+var 학생1 = new Student('Kim', 20);
+
+학생1.sayHi();
+// -> arrow function을 사용해서 this값이 window가 되어서.
+// *arrow function: this를 바깥에 있는 this를 그대로 사용하고 싶을 때 쓰는 함수
+
+// 4. 모든 array에 적용할 수 있는 함수
+// -> array 내에 있는 3이라는 값을 제거해주는 함수 만들기
+// var arr = [1,2,3];
+// arr.remove3();
+
+// console.log(arr); //[1,2]
+Array.prototype.remove3 = function(){
+  for (var i = 0; i < this.length; i++){
+    if (this[i] === 3) {
+      this.splice(i, 1);
+    }
+  }
+}
+var arr = [1,2,3,4];
+arr.remove3();
+
+console.log(arr); //[1,2,4]s
